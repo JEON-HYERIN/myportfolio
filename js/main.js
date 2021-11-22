0c60962d99d8866a15ed28cba17f0a678813b966
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-11-22 21:39:59 +0900

    최종

--- js/main.js
+++ js/main.js
@@ -4,55 +4,90 @@
 new Swiper('.main-practical__content.swiper.animation', {
   loop: true,
   // autoplay: true,
-  slidesPerView: 2,
   centeredSlides: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  effect: 'coverflow'
+  effect: 'coverflow',
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    }
+  }
 });
 new Swiper('.main-practical__content.swiper.hover', {
   loop: true,
   // autoplay: true,
-  slidesPerView: 2,
   centeredSlides: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  effect: 'coverflow'
+  effect: 'coverflow',
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    }
+  }
 });
 new Swiper('.main-practical__content.swiper.navigation', {
   loop: true,
   // autoplay: true,
-  slidesPerView: 2,
   centeredSlides: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  effect: 'coverflow'
+  effect: 'coverflow',
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    }
+  }
 });
 new Swiper('.main-practical__content.swiper.prototype', {
   loop: true,
   // autoplay: true,
-  slidesPerView: 2,
   centeredSlides: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  effect: 'coverflow'
+  effect: 'coverflow',
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    }
+  }
 });
 new Swiper('.main-practical__content.swiper.js', {
   loop: true,
   // autoplay: true,
-  slidesPerView: 2,
   centeredSlides: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  effect: 'coverflow'
+  effect: 'coverflow',
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    }
+  }
 });
